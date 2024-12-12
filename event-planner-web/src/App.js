53574830c35e97d1f446b5e0f15f4171534126a9
Troy Kopec <troykopec@gmail.com>
2024-12-11 21:36:23 -0500

    fix - homepage reroutting

--- event-planner-web/src/App.js
+++ event-planner-web/src/App.js
@@ -18,7 +18,7 @@ function App() {
         <Router>
           <Navbar />
           <Routes>
-            <Route path="/" element={<HomePage />} />
+            <Route path="/home" element={<HomePage />} />
             <Route path="/login" element={<AuthPage />} />
             <Route path="/signup" element={<SignupPage />} />
             <Route path="/my-tickets" element={<MyTicketsPage />} />
