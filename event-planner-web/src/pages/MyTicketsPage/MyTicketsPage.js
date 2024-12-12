60287564497375cc0d143eb1c56999c7fcb8251e
Troy Kopec <troykopec@gmail.com>
2024-12-12 00:08:34 -0500

    Update MyTicketsPage.js

--- event-planner-web/src/pages/MyTicketsPage/MyTicketsPage.js
+++ event-planner-web/src/pages/MyTicketsPage/MyTicketsPage.js
@@ -22,7 +22,7 @@ const MyTicketsPage = () => {
           return;
         }
 
-        const response = await axios.get("http://localhost:5000/api/tickets", {
+        const response = await axios.get(process.env.REACT_APP_BACKEND_URL + "api/tickets", {
           headers: {
             Authorization: `Bearer ${token}`,
           },
